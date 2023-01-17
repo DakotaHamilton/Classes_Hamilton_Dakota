2dfa4aa5bb2842139c2301b8fba2054b0c0a4684
DakotaHamilton <113059394+DakotaHamilton@users.noreply.github.com>
2023-01-17 10:36:22 -0600

    Add project files.

---
+++ Classes_Hamilton_Dakota/Greetings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Hamilton_Dakota
+{
+    class Greetings
+    {
+        /// <summary>
+        /// 
+        /// </summary> Welcome Class
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Dearest Adventurer, Welcome!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"> name of user </param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, what kind of adventures are in store today?");
+        }
+
+    }
+}
---
+++ Classes_Hamilton_Dakota/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes_Hamilton_Dakota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greetings Class
+            Greetings greetings = new Greetings();
+            
+            // Greetings Variable 
+            greetings.Welcome();
+
+            // Asked for name and user reply
+            Console.WriteLine("What is your name Adventurer?");
+            string userName = Console.ReadLine();
+
+            // Hello variable
+            greetings.Hello(userName);
+        }
+    }
+}
